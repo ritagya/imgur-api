28cb88cb0c43ef15fa18f444844b66c9e99a26c7
Jeff Killian <jeff.killian@atscale.com>
2017-09-06 09:40:29 -0700

    Initial Commit

---
+++ getClientId.js
@@ -0,0 +1,9 @@
+
+
+function getClientId(){
+  return new Promise(function(resolve, reject){
+        setTimeout(function() {
+           resolve({clientId:"e6bf8d8ea1e33cd"});
+        }, 2000);
+    });
+}  
---
+++ main.js
@@ -0,0 +1,15 @@
+/* We want to do the following
+
+
+ 1. Get info about an image with the ID n1xml
+ 2. Upload an image to imgur using the public API. The image to use is http://i68.tinypic.com/23upv91.gif.
+ 3. Get the size of the image when that's done. 
+ 4. Add the image to the page, with double the height and width
+ 5. If time, only return the id so it feels forced
+ 6. If time, do #2 in a separate call
+ 
+ getClientId() is an existing function which returns a promise which has the clientId. 
+ 
+ 
+ 
+  */

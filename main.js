b00d2e3465ae675ef409e022c1b6cd8817143730
Jeff Killian <jeff.killian@atscale.com>
2017-09-07 10:37:19 -0700

    Update instructions

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 /* We want to do the following
 
-http://imgur.com/n1xml
- 1. Get info about an image with the ID n1xml
+ 1. Get info about an image with the ID n1xml (just for reference, the url of that image is http://imgur.com/n1xml)
  2. Upload an image to imgur using the public API. The image to use is http://i68.tinypic.com/23upv91.gif.
  3. Get the size of the image when that's done. 
  4. Add the image to the page, with double the height and width
  5. If time, only return the id so it feels forced
  6. If time, do #2 in a separate call
  
- getClientId() is an existing function which returns a promise which has the clientId. 
+ Client ID is e6bf8d8ea1e33cd (imgur will need this)
  
  
  

76914cda39284fa00f62e6b90aaa429247977ec1
Jeff Killian <jeff.killian@atscale.com>
2017-09-27 09:30:46 -0700

    Update instructions

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 /* We want to do the following
 
+We'll be messing around with the imgur API - https://apidocs.imgur.com/
 
- 1. Get info about an image with the ID n1xml. You can hardcode in the clientId.
- 2. Do the same but use getClientId.js
+ 1. Get info about an image that has the hash (aka ID) n1xml
+ 2. Modify your code to utilize getClientId
  3. Upload an image to imgur using the public API. The image to use is http://i68.tinypic.com/23upv91.gif.
- 4. Get the size of the image when that's done. 
- 5. Add the image to the page, with double the height and width
+ 4. Add the image to the page, with double the height and width
  
- Client ID is: e6bf8d8ea1e33cd  
- getClientId() is an existing function which returns a promise which has the clientId above
- 
- 
+ clientId is e6bf8d8ea1e33cd
+
  
   */
